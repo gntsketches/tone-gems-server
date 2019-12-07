d91bdf4be3bac0ec7f6ef68f5bbcfaf543b498e4
Gregory Tippett <gntsketches@gmail.com>
2019-12-07 13:15:11 -0800

    added mongoose and pushed item to mongo

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+// models must be defined before passport is set up
+require('./models/User');
+// requiring passport to set it up; nothing is exported from it
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hey: 'buddy' });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000
 // environment variables injected by Heroku if deployed
 app.listen(PORT);
+
